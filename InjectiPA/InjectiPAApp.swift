ce03628b6a01ec660c1bd39d8ec4e5a2dcbcdb1d
TrialMacApp <ttpoxy@gmail.com>
2025-02-20 09:52:49 +0800

    Add online upgrade

--- InjectiPA/InjectiPAApp.swift
+++ InjectiPA/InjectiPAApp.swift
@@ -5,6 +5,7 @@
 //  Created by TrialMacApp on 2025-02-17.
 //
 
+import Sparkle
 import SwiftUI
 
 @main
@@ -12,6 +13,15 @@ struct InjectiPAApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .commands {
+            CommandGroup(after: .appInfo) {
+                CheckForUpdatesView(updater: UpdaterManager.shared.updater)
+            }
+            
+            CommandGroup(replacing: .help) {
+                Link("GitHub", destination: URL(string: "https://github.com/TrialAppleApp/InjectiPA/")!)
+            }
         }
     }
 }
